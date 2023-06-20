9a07382200b14607c674563208d665701155047f
Suraj Prabhu <surajprabhu19@gmail.com>
2023-06-20 09:49:00 +0530

    ResponseDto added for convenience

--- BookMyShow/bookmyshow/src/main/java/com/scaler/bookmyshow/dtos/ResponseStatusDto.java
+++ BookMyShow/bookmyshow/src/main/java/com/scaler/bookmyshow/dtos/ResponseStatusDto.java
@@ -1,11 +1,6 @@
 package com.scaler.bookmyshow.dtos;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ResponseStatusDto {
-    private String message;
-    private 
+public enum ResponseStatusDto {
+    SUCCESS,
+    FAILURE,
 }
